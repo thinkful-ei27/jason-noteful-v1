bbfb4f0f1970f142aa2dd7ecaa7986595f95a5d2
publikwerker <hello@publikwerks.com>
2018-12-17 12:01:13 -0800

    Create GET notes list endpoint

--- server.js
+++ server.js
@@ -8,14 +8,18 @@ console.log('Hello Noteful!');
 // INSERT EXPRESS APP CODE HERE...
 const express = require('express');
 
-const data = require('./db/notes');
+// const data = require('./db/notes');
 
 const app = express();
 
 // STATIC SERVER
 
 app.listen(8080, function() {
-    console.infor('server listening on ${this.address().port}');
+    console.info('server listening on ${this.address().port}');
 }).on('error', err => {
     console.error(err);
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(data);
 });
