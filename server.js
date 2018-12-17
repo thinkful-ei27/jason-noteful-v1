30d405669398a4c94960a36a191551035426eb7b
publikwerker <hello@publikwerks.com>
2018-12-17 11:45:09 -0800

    same as above, but this time i saved the files first...

--- server.js
+++ server.js
@@ -6,3 +6,16 @@ const data = require('./db/notes');
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+
+const data = require('./db/notes');
+
+const app = express();
+
+// STATIC SERVER
+
+app.listen(8080, function() {
+    console.infor('server listening on ${this.address().port}');
+}).on('error', err => {
+    console.error(err);
+});
