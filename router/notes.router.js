ec61a453c2ef9e8b95611ab2e5aa3fbb04d37296
publikwerker <hello@publikwerks.com>
2018-12-19 14:28:55 -0800

    started migrating things over to notes.router.js, but SNAFU

---
+++ router/notes.router.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = express.Router();
+
+const data = require('../db/notes');
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
+
+
+// Parse request body
+router.use(express.json());
+
+// HANDLE GET REQUESTS
+router.get('/api/notes/', (req, res, next) => {
+    const { searchTerm } = req.query;
+
+    notes.filter(searchTerm, (err, list) => {
+        if (err) {
+          return next(err); // goes to error handler
+        }
+        res.json(list); // responds with filtered array
+    });
+});
+
+router.get('/api/notes/:id', (req, res, next) => {
+    const id = (req.params.id);
+    notes.find(id, (err, item) => {
+        if (err) {
+          return next(err); // goes to error handler
+        };
+        if (item) {
+        res.json(item);
+        } else {
+            next();
+        }
+    });
+});
+
+// PUT NOTES
+router.put('/api/notes/:id', (req, res, next) => {
+    const id = req.params.id;
+  
+    /***** Never trust users - validate input *****/
+    const updateObj = {};
+    const updateFields = ['title', 'content'];
+  
+    updateFields.forEach(field => {
+      if (field in req.body) {
+        updateObj[field] = req.body[field];
+      }
+    });
+  
+    notes.update(id, updateObj, (err, item) => {
+      if (err) {
+        return next(err);
+      }
+      if (item) {
+        res.json(item);
+      } else {
+        next();
+      }
+    });
+  });
+
+//ERROR HANDLER
+router.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    res.status(404).json({ message: 'Not Found' });
+});
+
+router.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,99 +1,19 @@
 'use strict';
 
-// Load array of notes
-const data = require('./db/notes');
-const simDB = require('./db/simDB');
-const notes = simDB.initialize(data);
+const express = require('express');
 const { PORT } = require('./config');
 const morgan = require('morgan');
+const router = require('./router/notes.router');
 console.log('Hello Noteful!');
-
-
-
-
-// INSERT EXPRESS APP CODE HERE...
-const express = require('express');
 const app = express();
-
- // LOGGER MIDDLEWARE
-
- app.use(morgan(':method :url :status :response-time ms - :res[content-length]e'));
-
 
 // Create a static webserver
 app.use(express.static('public'));
 
-// Parse request body
-app.use(express.json());
+ // LOGGER MIDDLEWARE
+ app.use(morgan(':method :url :status :response-time ms - :res[content-length]e'));
 
-// HANDLE GET REQUESTS
-app.get('/api/notes/', (req, res, next) => {
-    const { searchTerm } = req.query;
-
-    notes.filter(searchTerm, (err, list) => {
-        if (err) {
-          return next(err); // goes to error handler
-        }
-        res.json(list); // responds with filtered array
-    });
-});
-
-app.get('/api/notes/:id', (req, res, next) => {
-    const id = (req.params.id);
-    notes.find(id, (err, item) => {
-        if (err) {
-          return next(err); // goes to error handler
-        };
-        if (item) {
-        res.json(item);
-        } else {
-            next();
-        }
-    });
-});
-
-// PUT NOTES
-app.put('/api/notes/:id', (req, res, next) => {
-    const id = req.params.id;
-  
-    /***** Never trust users - validate input *****/
-    const updateObj = {};
-    const updateFields = ['title', 'content'];
-  
-    updateFields.forEach(field => {
-      if (field in req.body) {
-        updateObj[field] = req.body[field];
-      }
-    });
-  
-    notes.update(id, updateObj, (err, item) => {
-      if (err) {
-        return next(err);
-      }
-      if (item) {
-        res.json(item);
-      } else {
-        next();
-      }
-    });
-  });
-
-//ERROR HANDLER
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    res.status(404).json({ message: 'Not Found' });
-});
-
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: err
-  });
-});
-
-// STATIC SERVER
+ app.use(router);
 app.listen(PORT, function() {
     console.info(`server listening on ${this.address().port}`);
 }).on('error', err => {
