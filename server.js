dbdf6ace1242d76e1d40534e8e99064e0edfdf32
publikwerker <hello@publikwerks.com>
2018-12-19 12:31:16 -0800

    added put function for notes

--- server.js
+++ server.js
@@ -17,6 +17,12 @@ const app = express();
  // LOGGER MIDDLEWARE
 app.use(logger);
 
+// Create a static webserver
+app.use(express.static('public'));
+
+// Parse request body
+app.use(express.json());
+
 // HANDLE GET REQUESTS
 app.get('/api/notes/', (req, res, next) => {
     const { searchTerm } = req.query;
@@ -43,22 +49,46 @@ app.get('/api/notes/:id', (req, res, next) => {
     });
 });
 
-
+// PUT NOTES
+app.put('/api/notes/:id', (req, res, next) => {
+    const id = req.params.id;
+  
+    /***** Never trust users - validate input *****/
+    const updateObj = {};
+    const updateFields = ['title', 'content'];
+  
+    updateFields.forEach(field => {
+      if (field in req.body) {
+        updateObj[field] = req.body[field];
+      }
+    });
+  
+    notes.update(id, updateObj, (err, item) => {
+      if (err) {
+        return next(err);
+      }
+      if (item) {
+        res.json(item);
+      } else {
+        next();
+      }
+    });
+  });
 
 //ERROR HANDLER
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     res.status(404).json({ message: 'Not Found' });
-  });
+});
 
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: err
-    });
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
   });
+});
 
 // STATIC SERVER
 app.listen(PORT, function() {
